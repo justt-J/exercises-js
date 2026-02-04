7d739fd3ffbf63bee12e62ab96a54b3bcbc4c4a2
jhay <vidanesjustine01@gmail.com>
2026-02-04 17:27:34 +0800

    updated subarray problem but only works for positive

--- Lesson1:Arrays/quiz/subarray_sum.js
+++ Lesson1:Arrays/quiz/subarray_sum.js
@@ -3,10 +3,23 @@
 
 function hasSubarraySum(arr, target) {
     for(let i=0; i < arr.length; i++){
+        let current_value = arr[i]
+        if(current_value === target){
+            return true
+        }
         for(let j = i + 1; j < arr.length; j++){
-            if ((arr[i] + arr[j]) === target) return true
+
+            if ((current_value + arr[j]) === target){
+                return true
+            }else if ((current_value + arr[j]) < target){
+                current_value = current_value + arr[j]
+            }else{
+                return false
+            }
         }
     }
     return false
 }
-hasSubarraySum([1,2,3,7], 6) //→ true   // [1,2,3]
+console.log(hasSubarraySum([8, 1, 2, 3, 2], 8))
+console.log(hasSubarraySum([1, 12, -9, 4], 8))
+console.log(hasSubarraySum([5, 6, -3], 8))
