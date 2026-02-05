ade13277b9713dc7d9b31a12bfd8c8e63042aca5
jhay <vidanesjustine01@gmail.com>
2026-02-05 16:18:31 +0800

    fixed subarray works with negative but still has error

---
+++ Lesson1:Arrays/quiz/moving_zero.js
@@ -0,0 +1,9 @@
+// Move Zeros to End
+// Modify array so all 0s are moved to the end while preserving order of others.
+
+
+function moveZeros(arr) {
+}
+
+
+moveZeros([0,1,0,3,12]) //→ [1,3,12,0,0]
--- Lesson1:Arrays/quiz/subarray_sum.js
+++ Lesson1:Arrays/quiz/subarray_sum.js
@@ -1,25 +1,44 @@
 // Subarray Sum
 // Return true if there exists a contiguous subarray whose sum is target.
 
-function hasSubarraySum(arr, target) {
-    for(let i=0; i < arr.length; i++){
-        let current_value = arr[i]
-        if(current_value === target){
-            return true
-        }
-        for(let j = i + 1; j < arr.length; j++){
-
-            if ((current_value + arr[j]) === target){
-                return true
-            }else if ((current_value + arr[j]) < target){
-                current_value = current_value + arr[j]
-            }else{
-                return false
-            }
-        }
+function getSum(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
     }
-    return false
+    return sum
 }
-console.log(hasSubarraySum([8, 1, 2, 3, 2], 8))
+
+function hasSubarraySum(arr, target){
+    while(arr.length > 0){
+        let sum = getSum(arr)
+        if(sum === target)return true
+        arr.pop()
+        sum = 0
+    }  
+    return false
+}   
+
+console.log(hasSubarraySum([1, 2, 3, 4], 5))
 console.log(hasSubarraySum([1, 12, -9, 4], 8))
 console.log(hasSubarraySum([5, 6, -3], 8))
+
+
+// function hasSubarraySum(arr, target) {
+//     for(let i=0; i < arr.length; i++){
+//         let current_value = arr[i]
+//         if(current_value === target){
+//             return true
+//         }
+//         for(let j = i + 1; j < arr.length; j++){
+//             if ((current_value + arr[j]) === target){
+//                 return true
+//             }else if ((current_value + arr[j]) < target){
+//                 current_value = current_value + arr[j]
+//             }else{
+//                 return false
+//             }
+//         }
+//     }
+//     return false
+// }
