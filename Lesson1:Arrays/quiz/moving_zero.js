030ebc63e9381327830b0a0db867f19301992a70
jhay <vidanesjustine01@gmail.com>
2026-02-05 17:10:40 +0800

    done moving_zero

--- Lesson1:Arrays/quiz/moving_zero.js
+++ Lesson1:Arrays/quiz/moving_zero.js
@@ -3,7 +3,19 @@
 
 
 function moveZeros(arr) {
+    let arr_zero = []
+    let j = 0
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            arr_zero[j] = arr[i]
+            arr.splice(arr.indexOf(arr[i]),1)
+            j++
+        }
+    }
+    for(let i = 0; i < arr_zero.length; i++){
+        arr.push(arr_zero[i])
+    }
+    console.log(arr)
 }
-
 
 moveZeros([0,1,0,3,12]) //→ [1,3,12,0,0]
