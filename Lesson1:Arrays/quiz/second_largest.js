9802b1f0bf407c04e7c6ce0b8bc24785c2695035
jhay <vidanesjustine01@gmail.com>
2026-02-10 15:00:25 +0800

    finished a task

--- Lesson1:Arrays/quiz/second_largest.js
+++ Lesson1:Arrays/quiz/second_largest.js
@@ -1,25 +1,13 @@
 // Second Largest
-
 // Return the second largest number in the array.
 
 function secondLargest(arr) {
     let target = arr[0]
+    let target_1 = 0
     for (let i = 0; i < arr.length; i++) {
         target = target < arr[i] ? arr[i] : target;
     }
+    return target_1
 }
 
 console.log(secondLargest([5,2,9,1]))
-
-// function secondLargest(arr) {
-//     let target = arr[0]
-//     for (let i = 0; i < arr.length; i++) {
-//         target = target < arr[i] ? arr[i] : target;
-//     }
-//     arr.splice(arr.indexOf(target),1)
-//     target = arr[0]
-//     for (let i = 0; i < arr.length; i++) {
-//         target = target < arr[i] ? arr[i] : target;
-//     }
-//     return target
-// }
